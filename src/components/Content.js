1ab9d63eba4ec8eca3b86dbaae6acdfad1f07dbb
Grayhood114 <Graysonzrelak@gmail.com>
2025-02-04 16:37:25 -0500

    ff
    Good

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <main>
+      <p>Once upon a time, a speedy hare challenged a slow but steady tortoise to a race. The hare, confident in his ability to win, decided to take a nap during the race.</p>
+      <p>Meanwhile, the tortoise continued moving steadily and eventually crossed the finish line while the hare was still asleep. The hare woke up too late and lost the race.</p>
+    </main>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Moral of the story: Dont be cocky and slow and steady wins the race.</p>
+    </footer>
+  );
+}
+
+export default Footer;
